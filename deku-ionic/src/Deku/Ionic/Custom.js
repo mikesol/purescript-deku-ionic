72ab89c22481fbdf6ec64eb294306c3ef7a3f662
Mike Solomon <mike.solomon@hey.com>
2024-05-01 05:14:14 +0300

    Removes explitivevs from JS file

--- deku-ionic/src/Deku/Ionic/Custom.js
+++ deku-ionic/src/Deku/Ionic/Custom.js
@@ -1,17 +1,14 @@
 export const customComponentImpl =
     (componentName) => (localProps) => (connectedHook) => (disconnectedHook) => (run) => () => {
-      console.log('doing');
     class CustomComponent extends HTMLElement {
       constructor() {
         super();
-        console.log('fuck');
         this.ionic$locals = undefined;
       }
       disconnectedCallback() {
         disconnectedHook(this.ionic$locals)();
       }
       connectedCallback() {
-        console.log('shit');
         this.ionic$locals = {};
         for (var i = 0; i < localProps.length; i++) {
           this.ionic$locals[localProps[i]] = this[localProps[i]];
@@ -20,6 +17,5 @@ export const customComponentImpl =
         connectedHook(this.ionic$locals)();
       }
     }
-    console.log('defining', componentName)
     window.customElements.define(componentName, CustomComponent);
   };
